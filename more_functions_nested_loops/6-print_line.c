42d2fc806d4d41301f8b2ac902d2ee6feecd396a
root <root@ip-10-42-56-78.ec2.internal>
2025-08-31 14:51:36 +0000

    fix

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,15 @@
 void print_line(int n)
 {
 int i;
-if (i <= 0)
+if (n <= 0)
 {
 _putchar('\n');
 }
 else
 {
 for (i = 0; i < n; i++)
-_putchar('_')
-_putchar('\n')
+_putchar('_');
+_putchar('\n');
 }
 }
 
