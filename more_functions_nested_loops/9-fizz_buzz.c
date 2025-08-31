e63d5d64923980b59fa1b768d5e62c5967fa294a
root <root@ip-10-42-56-78.ec2.internal>
2025-08-31 16:38:13 +0000

    fix

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,39 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer using _putchar
- * @n: integer to print
+ * main - prints numbers from 1 to 100 with FizzBuzz logic
+ *
+ * Return: Always 0
  */
-void print_number(int n)
+int main(void)
 {
-    if (n >= 10)
-        print_number(n / 10);
-    _putchar((n % 10) + '0');
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            _putchar('F'); _putchar('i'); _putchar('z'); _putchar('z');
+            _putchar('B'); _putchar('u'); _putchar('z'); _putchar('z');
+        }
+        else if (i % 3 == 0)
+        {
+            _putchar('F'); _putchar('i'); _putchar('z'); _putchar('z');
+        }
+        else if (i % 5 == 0)
+        {
+            _putchar('B'); _putchar('u'); _putchar('z'); _putchar('z');
+        }
+        else
+        {
+            print_number(i);
+        }
+
+        if (i < 100)
+            _putchar(' ');
+    }
+    _putchar('\n');
+
+    return (0);
 }
+
