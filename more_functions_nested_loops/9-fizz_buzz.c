7f812194ac880d30b5fa36233b89c9c66c99efc5
root <root@ip-10-42-56-78.ec2.internal>
2025-08-31 16:33:08 +0000

    feat

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using _putchar
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+    if (n >= 10)
+        print_number(n / 10);
+    _putchar((n % 10) + '0');
+}
