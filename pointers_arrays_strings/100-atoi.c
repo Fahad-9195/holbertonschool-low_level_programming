4f18c5afaf93cc0a241426617ddceddc64182178
root <root@ip-10-42-218-49.ec2.internal>
2025-09-01 07:24:03 +0000

    feat

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,20 +1,33 @@
-            found_digit = 1;
-            // Check for overflow before multiplying and adding
-            if (sign == 1)
-            {
-                if (result > (INT_MAX - (s[i] - '0')) / 10)
-                    return INT_MAX;
-            }
-            else
-            {
-                if (result > (-(INT_MIN + (s[i] - '0'))) / 10)
-                    return INT_MIN;
-            }
-            result = result * 10 + (s[i] - '0');
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string
+ *
+ * Return: the integer value, or 0 if no numbers are found
+ */
+int _atoi(char *s)
+{
+        int i = 0;
+        int sign = 1;
+        int result = 0;
+        int found_digit = 0;
+
+        while (s[i] != '\0')
+        {
+                if (s[i] == '-')
+                        sign *= -1;
+                else if (s[i] == '+')
+                        ; /* do nothing */
+                else if (s[i] >= '0' && s[i] <= '9')
+                {
+                        found_digit = 1;
+                        result = result * 10 + (s[i] - '0');
+                }
+                else if (found_digit)
+                        break;
+                i++;
         }
-        else if (found_digit)
-            break;
-        i++;
-    }
-    return (sign * result);
+
+        return (sign * result);
 }
