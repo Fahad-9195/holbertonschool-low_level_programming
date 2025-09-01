95adfc8669f7469228bb37601bcf46c290c3c28d
root <root@ip-10-42-218-49.ec2.internal>
2025-09-01 06:08:09 +0000

    feat

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+void print_rev(char *s)
+{
+    int length = 0;
+
+    while (s[length] != '\0')
+        length++;
+
+    for (length = length - 1; length >= 0; length--)
+        _putchar(s[length]);
+
+    _putchar('\n');
+}
