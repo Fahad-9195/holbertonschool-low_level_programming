6d2a703beeba0911a4fb6f78de7c0b75309a64c9
root <root@ip-10-42-218-49.ec2.internal>
2025-09-01 06:11:00 +0000

    feat

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: pointer to the string
+ *
+ * Return: nothing
+ */
 void print_rev(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    while (s[length] != '\0')
-        length++;
+	/* Count string length */
+	while (s[length] != '\0')
+		length++;
 
-    for (length = length - 1; length >= 0; length--)
-        _putchar(s[length]);
+	/* Print in reverse */
+	for (length = length - 1; length >= 0; length--)
+		_putchar(s[length]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
+
