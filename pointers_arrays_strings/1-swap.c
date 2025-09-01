6c701a62ee926b207554ff5b216762f38229c443
root <root@ip-10-42-218-49.ec2.internal>
2025-09-01 05:07:08 +0000

    feat

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+a = 5
+b = 10
+a = a + b
+b = a - b
+a = a - b
+ printf("a = %d, b = %d\n", a, b);
+    return 0;
+}
+
