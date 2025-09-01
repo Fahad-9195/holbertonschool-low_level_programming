d1dd678b1a217907df5c28a932c90932835379ee
root <root@ip-10-42-218-49.ec2.internal>
2025-09-01 07:32:25 +0000

    feat

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -18,17 +18,17 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] == '+')
-			; /* do nothing */
+			;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			found_digit = 1;
-			result = result * 10 + (s[i] - '0');
+			result = result * 10 + (sign > 0 ? (s[i] - '0') : -(s[i] - '0'));
 		}
 		else if (found_digit)
 			break;
 		i++;
 	}
 
-	return (sign * result);
+	return (result);
 }
 
