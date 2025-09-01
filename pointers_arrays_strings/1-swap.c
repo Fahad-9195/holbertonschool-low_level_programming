167ea332864f2b6fc47954923e7e43a49d15559e
root <root@ip-10-42-218-49.ec2.internal>
2025-09-01 05:08:01 +0000

    feat

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -7,11 +7,11 @@
  */
 void swap_int(int *a, int *b)
 {
-a = 5
-b = 10
-a = a + b
-b = a - b
-a = a - b
+a = 5;
+b = 10;
+a = a + b;
+b = a - b;
+a = a - b;
  printf("a = %d, b = %d\n", a, b);
     return 0;
 }
