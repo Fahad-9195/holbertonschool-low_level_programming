b376cab82622dd6a87ca0118af8b07999c253cbe
root <root@ip-10-42-218-49.ec2.internal>
2025-09-01 06:18:09 +0000

    feat

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string
+ *
+ * Return: nothing (void)
+ */
+void rev_string(char *s)
+{
+	int start = 0;
+	int end = 0;
+	char temp;
+	while (s[end] != '\0')
+		end++;
+	end = end - 1;
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}
