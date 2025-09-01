55c53e37376c261ee7e945082ecab19503c01e2c
root <root@ip-10-42-218-49.ec2.internal>
2025-09-01 07:16:55 +0000

    feat

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,4 @@
+#include "main.h"
 
 /**
  * _atoi - converts a string to an integer
@@ -7,27 +8,27 @@
  */
 int _atoi(char *s)
 {
-        int i = 0;
-        int sign = 1;
-        int result = 0;
-        int found_digit = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int found_digit = 0;
 
-        while (s[i] != '\0')
-        {
-                if (s[i] == '-')
-                        sign *= -1;
-                else if (s[i] == '+')
-                        ; /* ignore */
-                else if (s[i] >= '0' && s[i] <= '9')
-                {
-                        found_digit = 1;
-                        result = result * 10 + (s[i] - '0');
-                }
-                else if (found_digit)
-                        break;
-                i++;
-        }
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			; /* do nothing */
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (found_digit)
+			break;
+		i++;
+	}
 
-        return (sign * result);
+	return (sign * result);
 }
 
