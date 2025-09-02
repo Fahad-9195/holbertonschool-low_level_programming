0b066aef349ac037a0377ad341acd8e63ef37eb8
root <root@ip-10-42-134-16.ec2.internal>
2025-09-02 07:57:55 +0000

    fix

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -8,23 +8,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int d = 0;
-    int s = 0;
-
-    while (dest[d] != '\0')
-    {
-        d++;
-    }
-
-    while (s < n && src[s] != '\0')
-    {
-        dest[d] = src[s];
-        d++;
-        s++;
-    }
-
-    dest[d] = '\0';
-
-    return (dest);
+int d = 0;
+int s = 0;
+while (dest[d] != '\0')
+{
+d++;
+}
+while (s < n && src[s] != '\0')
+{
+dest[d] = src[s];
+d++;
+s++;
+}
+dest[d] = '\0';
+return (dest);
 }
 
