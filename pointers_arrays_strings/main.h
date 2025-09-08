5c78495cde0a3904d58e3c61b8f5401fa56ce895
root <root@ip-10-42-191-50.ec2.internal>
2025-09-08 06:41:07 +0000

    feat:2-strchr.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,7 +14,7 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-
+char *_strchr(char *s, char c);
 
 #endif
 
