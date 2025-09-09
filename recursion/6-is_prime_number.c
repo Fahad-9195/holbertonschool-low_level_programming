2f5a2f25c570d7931264dec40ce1b93ff184f9ea
root <root@ip-10-42-146-79.ec2.internal>
2025-09-09 05:49:39 +0000

    feat:

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * check_prime - helper to check if n is prime
+ * @n: number to check
+ * @divisor: current divisor to test
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int check_prime(int n, int divisor)
+{
+if (divisor * divisor > n)
+return (1);
+if (n % divisor == 0)
+return (0);
+return (check_prime(n, divisor + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+return (check_prime(n, 2));
+}
