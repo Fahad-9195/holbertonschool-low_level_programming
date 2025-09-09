bd549e9541ff5003ac09defb24a1c9da1216979b
root <root@ip-10-42-146-79.ec2.internal>
2025-09-09 05:43:59 +0000

    feat:

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,4 +1,21 @@
 #include "main.h"
+
+/**
+ * sqrt_helper - helper function to find sqrt recursively
+ * @n: number
+ * @guess: current guess
+ *
+ * Return: sqrt of n or -1
+ */
+int sqrt_helper(int n, int guess)
+{
+if (guess * guess == n)
+return (guess);
+if (guess * guess > n)
+return (-1);
+return (sqrt_helper(n, guess + 1));
+}
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to compute sqrt of
@@ -7,8 +24,8 @@
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    return (sqrt_helper(n, 0));
+if (n < 0)
+return (-1);
+return (sqrt_helper(n, 0));
 }
 
