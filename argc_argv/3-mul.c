80256f894597709e581d5db39721ee34454dac1a
root <root@ip-10-42-51-110.ec2.internal>
2025-09-10 05:13:00 +0000

    feat:

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - multiplies two numbers and prints the result
  * @argc: argument count
- * @argv: argument vector (expects two numbers at argv[1], argv[2])
+ * @argv: argument vector (expects two numbers at argv[1] and argv[2])
  *
- * Return: 0 on success, 1 if wrong number of arguments.
+ * Return: 0 on success, 1 if wrong number of arguments
  */
 int main(int argc, char *argv[])
+{
+	int a, b, product;
 
-{
-int a, b, product;
-if (argc != 3)
-{
-printf("Error\n");
-return (1);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	product = a * b;
+
+	printf("%d\n", product);
+	return (0);
 }
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-product = a * b;
-printf("%d\n", product);
-return (0);
-}
+
