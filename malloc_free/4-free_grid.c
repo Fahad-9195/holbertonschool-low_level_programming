99dae482e8e10d583fc1369bc62dc3ecf1488722
root <root@ip-10-42-107-46.ec2.internal>
2025-09-15 04:40:08 +0000

    feat:4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid created by alloc_grid
+ * @grid: pointer to the 2D grid
+ * @height: number of rows in the grid
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
