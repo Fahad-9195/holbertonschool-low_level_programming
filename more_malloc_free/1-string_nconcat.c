e1a6ecb1e5f803ac061d47ddfe34ac385cac2a9e
root <root@ip-10-42-233-79.ec2.internal>
2025-09-16 11:10:18 +0000

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,45 +7,39 @@
  * @s2: second string
  * @n: number of bytes from s2 to concatenate
  *
- * Return: pointer to newly allocated space containing s1
- *         followed by first n bytes of s2, null-terminated.
- *         If function fails, return NULL.
- *         If s1 or s2 is NULL, treat as empty string.
+ * Return: pointer to newly allocated space containing s1 then
+ * the first n bytes of s2, null-terminated; or NULL on failure.
+ * If s1 or s2 is NULL, treat it as an empty string.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *ptr;
-    unsigned int i, j, len1 = 0, len2 = 0;
+	char *ptr;
+	unsigned int i, j, len1 = 0, len2 = 0;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    /* Calculate lengths */
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+		n = len2;
 
-    /* allocate memory for s1 + n chars from s2 + '\0' */
-    ptr = malloc((len1 + n + 1) * sizeof(char));
-    if (ptr == NULL)
-        return (NULL);
+	ptr = malloc((len1 + n + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 
-    /* copy s1 */
-    for (i = 0; i < len1; i++)
-        ptr[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
 
-    /* copy n bytes from s2 */
-    for (j = 0; j < n; j++)
-        ptr[i + j] = s2[j];
+	for (j = 0; j < n; j++)
+		ptr[i + j] = s2[j];
 
-    ptr[i + j] = '\0';
+	ptr[i + j] = '\0';
 
-    return (ptr);
+	return (ptr);
 }
-
