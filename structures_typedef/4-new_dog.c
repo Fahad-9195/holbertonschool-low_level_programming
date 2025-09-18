5c16cd54bd0323b8cfce055a269370d9e46bed69
root <root@ip-10-42-212-198.ec2.internal>
2025-09-18 14:23:37 +0000

    fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,56 +1,70 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/* local helpers (لا تُحتسب ضمن طول new_dog) */
+static int _strlen_local(const char *s)
+{
+	int len = 0;
+
+	if (s == NULL)
+		return (0);
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+static char *_strdup_local(const char *s)
+{
+	char *p;
+	int i, len = _strlen_local(s);
+
+	p = malloc((len + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		p[i] = s[i];
+	p[len] = '\0';
+	return (p);
+}
+
 /**
  * new_dog - creates a new dog
- * @name: dog's name
+ * @name: dog's name (copied)
  * @age: dog's age
- * @owner: dog's owner
+ * @owner: dog's owner (copied)
  *
- * Return: pointer to new dog_t; NULL if function fails
+ * Return: pointer to new dog_t, or NULL on failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
 	char *nname, *nowner;
-	int i, len_name = 0, len_owner = 0;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	while (name[len_name] != '\0')
-		len_name++;
-	while (owner[len_owner] != '\0')
-		len_owner++;
-
-	ndog = malloc(sizeof(dog_t));
-	if (ndog == NULL)
-		return (NULL);
-
-	nname = malloc((len_name + 1) * sizeof(char));
+	nname = _strdup_local(name);
 	if (nname == NULL)
-	{
-		free(ndog);
 		return (NULL);
-	}
-	for (i = 0; i < len_name; i++)
-		nname[i] = name[i];
-	nname[i] = '\0';
 
-	nowner = malloc((len_owner + 1) * sizeof(char));
+	nowner = _strdup_local(owner);
 	if (nowner == NULL)
 	{
 		free(nname);
-		free(ndog);
 		return (NULL);
 	}
-	for (i = 0; i < len_owner; i++)
-		nowner[i] = owner[i];
-	nowner[i] = '\0';
+
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+	{
+		free(nname);
+		free(nowner);
+		return (NULL);
+	}
 
 	ndog->name = nname;
 	ndog->age = age;
 	ndog->owner = nowner;
-
 	return (ndog);
 }
