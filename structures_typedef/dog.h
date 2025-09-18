c4e51c3bf80ad55d939d7847a62365759318bf04
root <root@ip-10-42-212-198.ec2.internal>
2025-09-18 13:42:13 +0000

    feat

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure that stores dog information
+ * @name: name of the dog (string)
+ * @age: age of the dog (float)
+ * @owner: owner of the dog (string)
+ *
+ * Description: Defines a new type struct dog with three members
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
