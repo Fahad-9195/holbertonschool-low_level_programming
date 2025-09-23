bf0b3370b861240718917b294ce960b58cc3aa92
root <root@ip-10-42-100-183.ec2.internal>
2025-09-23 07:46:23 +0000

    fix: 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,47 +3,43 @@
 
 /**
  * print_all - prints anything based on a format string
- * @format: list of types of arguments passed to the function
- *
- * c: char
- * i: integer
- * f: float
- * s: char * (if NULL prints (nil))
- * any other char is ignored
+ * @format: list of types of args (c,i,f,s)
  */
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	int i = 0;
-	char *str;
+	unsigned int i = 0;
 	char *sep = "";
+	char *s;
 
 	va_start(ap, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		switch (format[i])
 		{
+		case 'c':
 			printf("%s%c", sep, va_arg(ap, int));
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%s%d", sep, va_arg(ap, int));
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", sep, va_arg(ap, double));
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(ap, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", sep, str);
-		}
-		if (format[i] == 'c' || format[i] == 'i' ||
-		    format[i] == 'f' || format[i] == 's')
 			sep = ", ";
+			break;
+		case 'i':
+			printf("%s%d", sep, va_arg(ap, int));
+			sep = ", ";
+			break;
+		case 'f':
+			printf("%s%f", sep, va_arg(ap, double));
+			sep = ", ";
+			break;
+		case 's':
+			s = va_arg(ap, char *);
+			if (s == NULL) /* 1st and only needed if */
+				s = "(nil)";
+			printf("%s%s", sep, s);
+			sep = ", ";
+			break;
+		default:
+			break;
+		}
 		i++;
 	}
 
