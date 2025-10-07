bc78947e31739d65f92820f080f6eaeb79c903d2
root <root@ip-10-42-50-52.ec2.internal>
2025-10-07 10:20:37 +0000

    fix:Betty coding style

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,12 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @str: string (malloc'ed)
  * @len: length of the string
  * @next: points to the next node
  *
@@ -14,9 +13,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 /* Function prototypes */
