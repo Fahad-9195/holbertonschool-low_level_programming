28051d449e6921c276e8be0f45cbe52b712f0c5a
root <root@ip-10-42-50-52.ec2.internal>
2025-10-07 10:46:06 +0000

    fix: the code

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
@@ -21,7 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (dup_str == NULL)
 		return (NULL);
 
-	while (str[len])
+	while (str[len] != '\0')
 		len++;
 
 	new_node = malloc(sizeof(list_t));
@@ -32,7 +33,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->str = dup_str;
-	new_node->len = len;
+	new_node->len = (unsigned int)len;
 	new_node->next = *head;
 	*head = new_node;
 
