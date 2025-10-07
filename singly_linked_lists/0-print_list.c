98937e4b3ea6fa231c5404d12133d3a797472990
root <root@ip-10-42-50-52.ec2.internal>
2025-10-07 10:16:51 +0000

    fix : the code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,23 +1,26 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0; // عدد العقد
+    size_t count = 0; /* عدد العقد */
 
-    // طالما h ليست NULL (يعني لسه فيه عناصر)
-    while (h)
+    while (h != NULL)
     {
-        // لو النص فاضي (NULL)
         if (h->str == NULL)
             printf("[0] (nil)\n");
         else
             printf("[%u] %s\n", h->len, h->str);
 
-        // انتقل للعقدة التالية
         h = h->next;
-        count++; // زِد العداد
+        count++; /* زيادة العداد لكل عقدة */
     }
 
-    return (count); // رجّع عدد العقد
+    return (count); /* إرجاع عدد العقد */
 }
