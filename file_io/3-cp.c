d18aa30f82f6d6d29f8e9beeade7bf7a2f403439
root <root@ip-10-42-246-97.ec2.internal>
2025-10-08 11:50:35 +0000

    fix: Betty coding style

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,24 +30,37 @@ static void close_fd(int fd)
 
 static void write_chunk(int fd_to, char *buf, ssize_t r, const char *file_to)
 {
-	ssize_t total = 0, w;
+	ssize_t total = 0;
+	ssize_t w;
 
 	while (total < r)
 	{
 		w = write(fd_to, buf + total, r - total);
 		if (w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				file_to);
 			exit(99);
 		}
 		total += w;
 	}
 }
 
-static void copy_rest(int fd_from, int fd_to, char *buf,
-		      const char *file_from, const char *file_to)
+static void copy_rest(int fd_from, const char *file_to, char *buf,
+		      ssize_t r_first, const char *file_from)
 {
+	int fd_to;
 	ssize_t r;
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	if (r_first > 0)
+		write_chunk(fd_to, buf, r_first, file_to);
 
 	r = read(fd_from, buf, BUFSIZE);
 	while (r > 0)
@@ -59,13 +72,16 @@ static void copy_rest(int fd_from, int fd_to, char *buf,
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 			file_from);
+		close_fd(fd_to);
 		exit(98);
 	}
+
+	close_fd(fd_to);
 }
 
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
+	int fd_from;
 	ssize_t r_first;
 	char *buf;
 
@@ -86,7 +102,6 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	/* جرّب القراءة أولاً لضمان كود 98 عند فشل read */
 	r_first = read(fd_from, buf, BUFSIZE);
 	if (r_first == -1)
 	{
@@ -97,22 +112,9 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		free(buf);
-		close_fd(fd_from);
-		exit(99);
-	}
-
-	if (r_first > 0)
-		write_chunk(fd_to, buf, r_first, argv[2]);
-
-	copy_rest(fd_from, fd_to, buf, argv[1], argv[2]);
+	copy_rest(fd_from, argv[2], buf, r_first, argv[1]);
 
 	free(buf);
 	close_fd(fd_from);
-	close_fd(fd_to);
 	return (0);
 }
