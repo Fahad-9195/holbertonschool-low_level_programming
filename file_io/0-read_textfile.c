97c09d6e06eef7a23b1e2ea9adccd036281d44ab
root <root@ip-10-42-246-97.ec2.internal>
2025-10-08 09:47:04 +0000

    fix: edite Betty coding style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -56,3 +56,46 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	return (bytes_written);
 }
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to STDOUT
+ * @filename: file name
+ * @letters: max bytes to read and print
+ * Return: bytes printed, or 0 on any failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r, w;
+	char *buf;
+
+	if (!filename || !letters)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
+
+	r = read(fd, buf, letters);
+	if (r <= 0)
+	{
+		free(buf), close(fd);
+		return (0);
+	}
+
+	w = write(STDOUT_FILENO, buf, r);
+	if (w != r)
+		w = 0;
+
+	free(buf);
+	close(fd);
+	return (w);
+}
